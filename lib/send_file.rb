4e0d461477089e73ef244b4f6d5b55491ff6b117
Tobias Lütke <tobi@jadedpixel.com>
2009-08-17 11:35:08 -0400

    deliver content length to browser

--- lib/send_file.rb
+++ lib/send_file.rb
@@ -13,6 +13,6 @@ module SendFile
     cache_control = file.respond_to?(:cache_control) ? file.cache_control : 'public, max-age: 31557600'
     content_type  = file.respond_to?(:content_type)  ? file.content_type || ContentTypes[file.ext] || raise(ArgumentError, 'illegal content type') : 'application/octet-stream'
     
-    [200, {'Content-Type' => content_type, 'Cache-Control' => cache_control}, [file.content]]
+    [200, {'Content-Type' => content_type, 'Cache-Control' => cache_control, 'Content-Length' => file.content.length}, [file.content]]
   end  
 end
